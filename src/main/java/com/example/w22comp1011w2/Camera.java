1cb0c6cbe118348be78f1bc0d8275754cb368908
kchalgotra <chalgotra23kashish@gmail.com>
2022-01-17 18:42:01 -0500

    Initial commit fxml for camera

---
+++ src/main/java/com/example/w22comp1011w2/Camera.java
@@ -0,0 +1,82 @@
+package com.example.w22comp1011w2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Camera {
+    private int resolution;
+    private String make, model;
+    private boolean slr;
+    private double price;
+
+    public Camera(String make, String model, int res, boolean slr, double price) {
+        setResolution(res);
+        setMake(make);
+        setModel(model);
+        setSlr(slr);
+        setPrice(price);
+    }
+
+    public int getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(int resolution) {
+        if (resolution >= 2 && resolution <= 100)
+            this.resolution = resolution;
+        else
+            throw new IllegalArgumentException("resolution should be between 2-100");
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    /**
+     * This method will validate that the manufacturer is either Canon, Nikon, Sony or FujiFilm
+     * @param make - the manufacturer of camera
+     */
+    public void setMake(String make) {
+        List<String> validMakes = Arrays.asList("Canon", "Nikon", "Sony", "Fujifilm");
+        if (validMakes.contains(make))
+            this.make = make;
+        else
+            throw new IllegalArgumentException("Make must be in the list of: " + validMakes);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * Valid models are Canon, Nikon or Sony
+     *
+     * @param model
+     */
+    public void setModel(String model) {
+        if(!model.isBlank())
+        this.model = model;
+        else
+            throw new IllegalArgumentException("Model can not be blank");
+    }
+
+    public boolean isSlr() {
+        return slr;
+    }
+
+    public void setSlr(boolean slr) {
+        this.slr = slr;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price >= 10 && price <= 5000)
+            this.price = price;
+        else
+            throw new IllegalArgumentException("price must be in the range 10-5000");
+    }
+}
+
---
+++ src/main/java/com/example/w22comp1011w2/CreateCameraViewController.java
@@ -0,0 +1,4 @@
+package com.example.w22comp1011w2;
+
+public class CreateCameraViewController {
+}
